d06f34e68008b9eaf0175ddd68709d8dc6d60ef5
rakib <phi.rakib@gmail.com>
2021-05-26 05:38:50 +0600

    replaced header tag by todoPage component

--- src/App.js
+++ src/App.js
@@ -1,10 +1,11 @@
 import React from 'react';
 import './App.css';
+import TodoPage from './features/todos/TodoPage';
 
 function App() {
   return (
     <div className="App">
-      <h1>Todo List</h1>
+      <TodoPage />
     </div>
   );
 }
